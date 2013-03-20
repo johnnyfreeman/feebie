d58f539e85cb20bfd75cfc4301d10128a314c8b7
Johnny Freeman <prsjohnny@gmail.com>
2013-03-20 18:28:56 -0400

    capified

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,13 @@ require 'capistrano/ext/multistage'
 set :stages, ["staging", "production"]
 set :default_stage, "staging"
 
-set :application, "fee_finder"
+set :application, "fee-finder"
 set :scm, :git
 set :repository, "johnny@198.61.175.65:repo/fee-finder.git"
-set :scm_passphrase, "29483270"
+set :scm_passphrase, ""
 set :user, "johnny"
+# set :ssh_key, "id_rsa"
 # set :use_sudo, true
 default_run_options[:pty] = true
+# set :ssh_options, { :forward_agent => true }
+# set :deploy_via, :remote_cache
