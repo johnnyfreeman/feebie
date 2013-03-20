e8d513fd6831ad5ca1de210e0ccd8c6115db08ac
Johnny Freeman <prsjohnny@gmail.com>
2013-03-20 17:50:41 -0400

    capified

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,5 +6,5 @@ set :default_stage, "staging"
 set :application, "fee_finder"
 set :scm, :git
 set :repository, "johnny@198.61.175.65:repo/fee-finder.git"
-set :scm_passphrase, ""
+set :scm_passphrase, "29483270"
 set :user, "johnny"
