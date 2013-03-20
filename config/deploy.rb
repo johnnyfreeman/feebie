41fc84ac12d0fcffc8b6180e94982ac7f0a4e02c
Johnny Freeman <prsjohnny@gmail.com>
2013-03-20 17:39:58 -0400

    capified

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,9 @@
-set :application, "Fee Finder"
+require 'capistrano/ext/multistage'
+
+set :stages, ["staging", "production"]
+set :default_stage, "staging"
+
+set :application, "fee_finder"
 set :scm, :git
 set :repository, "johnny@198.61.175.65:repo/fee-finder.git"
 set :scm_passphrase, ""
