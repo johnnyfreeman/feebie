9159bd1e397b8d7090723827bcc57250b879507b
Johnny Freeman <prsjohnny@gmail.com>
2013-03-26 14:16:04 -0400

    adding analytics tracking to all routes

--- public/assets/app/js/router.js
+++ public/assets/app/js/router.js
@@ -3,9 +3,15 @@
 FF.Router = Marionette.AppRouter.extend({
 
   appRoutes: {
-    '':      'displaySearch',
-    'help':  'displayHelp',
-    ':code': 'lookUpCode'
-  }
+		'':      'displaySearch',
+		'help':  'displayHelp',
+		':code': 'lookUpCode'
+  },
+
+	// update google analytics
+  navigate: function(fragment, options) {
+  	_gaq.push(['_trackPageview', fragment]);
+	  return Marionette.AppRouter.prototype.navigate.call(this, fragment, options);
+	}
 
 });
