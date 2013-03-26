6fa0e1fd611ff09edaf382f526c1f35ba00bbe68
Johnny Freeman <prsjohnny@gmail.com>
2013-03-26 13:12:18 -0400

    configured grunt to minify all scripts into one

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,37 @@ module.exports = function(grunt) {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
+      // build: {
+      //   src: 'public/assets/app/js/<%= pkg.name %>.js',
+      //   dest: 'build/<%= pkg.name %>.min.js'
+      // }
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        files: {
+          'public/assets/app/js/<%= pkg.name %>.min.js': [
+            'public/assets/jquery/jquery-1.9.1.js',
+            'public/assets/underscore/underscore.js',
+            'public/assets/backbone/backbone.js',
+            'public/assets/marionette/backbone.marionette.min.js',
+            'public/assets/zclip/zclip.min.js',
+            'public/assets/bootstrap/js/bootstrap.js',
+            'public/assets/flat-ui/js/jquery.placeholder.js',
+            'public/assets/greensock-v12-js/src/minified/TweenMax.js',
+
+            'public/assets/app/js/application.js',
+            'public/assets/app/js/models/code.js',
+            'public/assets/app/js/models/fees.js',
+            'public/assets/app/js/collections/code.js',
+            'public/assets/app/js/collections/fees.js',
+            'public/assets/app/js/regions/main.js',
+            'public/assets/app/js/views/code-item.js',
+            'public/assets/app/js/views/options-item.js',
+            'public/assets/app/js/views/fees-item.js',
+            'public/assets/app/js/views/notification-item.js',
+            'public/assets/app/js/views/search-form-item.js',
+            'public/assets/app/js/controller.js',
+            'public/assets/app/js/router.js'
+          ]
+        }
       }
     }
   });
